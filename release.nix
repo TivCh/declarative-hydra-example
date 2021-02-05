5f14e18ad4c814a0cc02cfa487d5f7d703561d67
TivCh <09200078@unmsm.edu.pe>
2021-02-05 17:41:38 -0500

    hey

--- release.nix
+++ release.nix
@@ -1,14 +1,12 @@
 { nixpkgs }:
 let
   pkgs = import nixpkgs {};
-  fil = import <bla> {};
 in
 {
   hello = pkgs.hello;
   hey = pkgs.runCommand "hey" {} ''
     ls -a
     ls
-    cat ${fil}
     touch $out
   '';
 }
